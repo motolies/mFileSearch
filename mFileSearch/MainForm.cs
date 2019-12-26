6b1cbbd69da3abfab32fffb36f3a31b68921038d
motolies <knw1234@gmail.com>
2019-12-26 22:30:11 +0900

    열린 파일이 잠겨있으면 오류나는거 수정

--- mFileSearch/MainForm.cs
+++ mFileSearch/MainForm.cs
@@ -185,7 +185,11 @@ namespace mFileSearch
 
                 int lineNumber = 1;
 
-                foreach (string line in File.ReadLines(file, GetFileEncoding(file)))
+                Encoding encoding = GetFileEncoding(file);
+                if (encoding == null)
+                    return;
+
+                foreach (string line in File.ReadLines(file, encoding))
                 {
                     if (isStop)
                         break;
@@ -215,30 +219,36 @@ namespace mFileSearch
         //인코딩 확인
         public static Encoding GetFileEncoding(string srcFile)
         {
-            // *** Use Default of Encoding.Default (Ansi CodePage)
-            Encoding enc = Encoding.Default;
-            // *** Detect byte order mark if any - otherwise assume default
-            byte[] buffer = new byte[10];
-            FileStream file = new FileStream(srcFile, FileMode.Open, FileAccess.Read, FileShare.Read);
-            file.Read(buffer, 0, 10);
-            file.Close();
-            if (buffer[0] == 0xef && buffer[1] == 0xbb && buffer[2] == 0xbf)
-                enc = Encoding.UTF8; //with BOM
-            else if (buffer[0] == 0xfe && buffer[1] == 0xff)
-                enc = Encoding.Unicode;
-            else if (buffer[0] == 0 && buffer[1] == 0 && buffer[2] == 0xfe && buffer[3] == 0xff)
-                enc = Encoding.UTF32;
-            else if (buffer[0] == 0x2b && buffer[1] == 0x2f && buffer[2] == 0x76)
-                enc = Encoding.UTF7;
-            else if (buffer[0] == 0xFE && buffer[1] == 0xFF)
-                // 1201 unicodeFFFE Unicode (Big-Endian)
-                enc = Encoding.GetEncoding(1201);
-            else if (buffer[0] == 0xFF && buffer[1] == 0xFE)
-                // 1200 utf-16 Unicode
-                enc = Encoding.GetEncoding(1200);
-            else if (ValidateUTF8WithoutBOM(srcFile))
-                enc = new UTF8Encoding(false);
-            return enc;
+            try
+            {
+                // *** Use Default of Encoding.Default (Ansi CodePage)
+                Encoding enc = Encoding.Default;
+                // *** Detect byte order mark if any - otherwise assume default
+                byte[] buffer = new byte[10];
+                FileStream file = new FileStream(srcFile, FileMode.Open, FileAccess.Read, FileShare.Read);
+                file.Read(buffer, 0, 10);
+                file.Close();
+                if (buffer[0] == 0xef && buffer[1] == 0xbb && buffer[2] == 0xbf)
+                    enc = Encoding.UTF8; //with BOM
+                else if (buffer[0] == 0xfe && buffer[1] == 0xff)
+                    enc = Encoding.Unicode;
+                else if (buffer[0] == 0 && buffer[1] == 0 && buffer[2] == 0xfe && buffer[3] == 0xff)
+                    enc = Encoding.UTF32;
+                else if (buffer[0] == 0x2b && buffer[1] == 0x2f && buffer[2] == 0x76)
+                    enc = Encoding.UTF7;
+                else if (buffer[0] == 0xFE && buffer[1] == 0xFF)
+                    // 1201 unicodeFFFE Unicode (Big-Endian)
+                    enc = Encoding.GetEncoding(1201);
+                else if (buffer[0] == 0xFF && buffer[1] == 0xFE)
+                    // 1200 utf-16 Unicode
+                    enc = Encoding.GetEncoding(1200);
+                else if (ValidateUTF8WithoutBOM(srcFile))
+                    enc = new UTF8Encoding(false);
+                return enc;
+            }
+            catch {
+                return null;
+            }
         }
         private static bool ValidateUTF8WithoutBOM(string filePath)
         {
